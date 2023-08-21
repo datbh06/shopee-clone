ebc90b438e99245d4609b4e4f3ec8fbf461dd406
datbh <bdat1606@gmail.com>
2023-08-21 08:28:41 +0700

    Initial commit

---
+++ shopee-clone/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ProductDetailSection from "./Components/ProductDetailSection.jsx";
+
+function App() {
+
+
+    return (<div className="max-w-7xl mx-auto p-8">
+            <ProductDetailSection/>
+        </div>)
+}
+
+export default App
